ce489f3cfb961fc0d0d960c7d80834bc7ae9c364
Glen Paul Marquez <glenpaulmarquez01@gmail.com>
2026-09-02 17:40:38 +0800

    Fixed the Issue on Not Working Clicking the Images on Certificates and add Cursor Pointer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,16 @@
-<<<<<<< HEAD
 document.oncontextmenu = () => {
     return false;
 }
-
 document.onkeydown = e => {
-    if(e.key === 'F12' || e.ctrlKey && e.key === 'u'.toLowerCase()){
-        return false;
-    }
-
     if(e.ctrlKey && e.key === 'c'.toLowerCase() || e.ctrlKey && e.key === 'v'.toLowerCase()){
         return false;
     }
 }
 
-document.querySelectorAll('.achievement-card img, .project-card img').forEach(img => {
-    img.addEventListener('click', () => {
-        window.open(img.src, "_blank");
-    })
-=======
-document.oncontextmenu = () => {
-    return false;
-}
-
-document.onkeydown = e => {
-    if(e.key === 'F12' || e.ctrlKey && e.key === 'u'.toLowerCase()){
-        return false;
-    }
-
-    if(e.ctrlKey && e.key === 'c'.toLowerCase() || e.ctrlKey && e.key === 'v'.toLowerCase()){
-        return false;
-    }
-}
+// Full Viewing the Image Since You Can't Right Click It
 
 document.querySelectorAll('.achievement-card img, .project-card img').forEach(img => {
     img.addEventListener('click', () => {
         window.open(img.src, "_blank");
     })
->>>>>>> my-temporary-fix
 });
